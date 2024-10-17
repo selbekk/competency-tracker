47e2f99169ff28b289168ce693da43440a0f35d7
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-10-17 22:07:19 +0200

    Make signin and signup pages redirect if logged in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,13 @@
 import { SignInComponent } from "@/components/sign-in";
+import { createClient } from "@/lib/supabase/client";
+import { redirect } from "next/navigation";
 import { login } from "./action";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  if (data?.user) {
+    redirect("/tracker");
+  }
   return <SignInComponent login={login} />;
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,13 @@
 import { SignUpComponent } from "@/components/sign-up";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 import { signup } from "./action";
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  if (data?.user) {
+    redirect("/tracker");
+  }
   return <SignUpComponent signup={signup} />;
 }
